88957df062d12fefd587d4cf8506c5cba9310c8c
2401325019 <2401325019gt@cyber-u.ac.jp>
2026-07-10 06:27:42 +0900

    change url

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -5,7 +5,7 @@ const cors = require('cors'); // corsミドルウェアを追加
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "mongodb://cu2401325019:b6dZS9r_n@ac-qgqdnl5-shard-00-00.cw69zcu.mongodb.net:27017,ac-qgqdnl5-shard-00-01.cw69zcu.mongodb.net:27017,ac-qgqdnl5-shard-00-02.cw69zcu.mongodb.net:27017/?ssl=true&replicaSet=atlas-ivz1c0-shard-0&authSource=admin&appName=Cluster0";
+const uri = "**********";
 const client = new MongoClient(uri);
 
 // corsミドルウェアを使用
